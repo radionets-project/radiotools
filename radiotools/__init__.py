d08a241c75ec204ee9423fac2ab130233973c1f7
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-05-31 18:15:05 +0200

    Add version to project

---
+++ radiotools/__init__.py
@@ -0,0 +1,9 @@
+"""
+radiotools - A collection of tools for use in radio astronomy.
+
+Licensed under a MIT style license - see LICENSE
+"""
+
+from .version import __version__
+
+__all__ = ["__version__"]
