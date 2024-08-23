c73b717d37d522fea33b37d621080681c0c483f7
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-08-23 17:53:14 +0200

    Add WSClean wrapper class

---
+++ radiotools/cleaning/__init__.py
@@ -0,0 +1,3 @@
+from .clean import WSClean
+
+__all__ = ["WSClean"]
---
+++ radiotools/cleaning/clean.py
@@ -0,0 +1,155 @@
+import subprocess as sp
+from pathlib import Path
+
+import toml
+
+
+class WSClean:
+    def __init__(
+        self,
+        ms: str | Path,
+        clean_config: dict | str | Path = None,
+        create_skymodel: bool = False,
+        save_config: bool | str | Path = False,
+    ) -> None:
+        """Wrapper class for WSClean.
+
+        Parameters
+        ----------
+        ms : str or Path
+            Path to the measurement set.
+        clean_config : dict or str or Path
+            Either a dict containing the wsclean options or
+            a path to a toml file containing the options.
+            See note.
+        create_skymodel : bool, optional
+            Whether to create a skymodel before cleaning the image.
+            Default: False.
+        save_config : bool or str or Path, optional
+            If True, save the config to a file relative to the file
+            this class is called from. If given a path, the config
+            is saved to that location instead. Default: False.
+
+        Notes
+        -----
+        A config may look like the following:
+
+        >>> clean_params = {
+        >>>     'multiscale': True,
+        >>>     'mgain': 0.5,
+        >>>     'file_name': 'path/to/measurement_set',
+        >>>     'data_column': 'DATA',
+        >>>     'gain': 0.03,
+        >>>     'weight': 'briggs 0',
+        >>>     'mf_weighting': False,
+        >>>     'size': [1024, 1024],
+        >>>     'scale': '0.1masec',
+        >>>     'pol': 'Q',
+        >>>     'niter': 5000000,
+        >>>     'auto_threshold': 0.5,
+        >>>     'auto_mask': 3,
+        >>>     'padding': 1.3,
+        >>>     'mem': 30,
+        >>>     'verbose': True,
+        >>> }
+        """
+        if not isinstance(clean_config, (dict, str, Path)):
+            raise ValueError(
+                "Please provide EITHER a dict object or a path"
+                " to an existing configuration toml file."
+            )
+
+        if not Path(ms).is_dir():
+            raise OSError(f"Measurement set {Path(ms).absolute()} does not exist.")
+
+        self.ms = ms
+
+        if isinstance(clean_config, (str, Path)):
+            if not Path(clean_config).is_file():
+                raise OSError(f"File {Path(clean_config).absolute()} does not exist.")
+
+            with open(clean_config) as toml_file:
+                clean_config = toml.load(toml_file)
+
+        if save_config:
+            self._save_config(clean_config, save_config)
+
+        clean_config["name"] = clean_config.pop("file_name")
+        clean_config["data-column"] = clean_config.pop("data_column")
+        clean_config["auto-threshold"] = clean_config.pop("auto_threshold")
+        clean_config["auto-mask"] = clean_config.pop("auto_mask")
+
+        if isinstance(clean_config["size"], int):
+            clean_config["size"] = [clean_config["size"]] * 2
+
+        if clean_config["mf_weighting"] is False:
+            del clean_config["mf_weighting"]
+            clean_config["no-mf-weighting"] = ""
+        else:
+            clean_config["mf-weighting"] = ""
+
+        if clean_config["verbose"] is False:
+            del clean_config["verbose"]
+            clean_config["quiet"] = ""
+        else:
+            del clean_config["verbose"]
+
+        if clean_config["multiscale"] is False:
+            del clean_config["multiscale"]
+        else:
+            clean_config["multiscale"] = ""
+
+        self.clean_config = clean_config
+
+        if create_skymodel:
+            self.skymodel_kwargs = clean_config.copy()
+            self.skymodel_kwargs["name"] += f"_{self.skymodel_kwargs['pol']}_skymodel"
+            self.skymodel_kwargs["data-column"] = "DATA"
+            self.skymodel_kwargs["auto-threshold"] = 1
+            self.skymodel_kwargs["auto-mask"] = 3
+            self.skymodel_kwargs["niter"] = 1000000
+
+            self.create_skymodel()
+
+    def create_skymodel(self) -> None:
+        """Creates the skymodel using WSClean if ``create_skymodel``
+        is set to ``True`` when initializing the class.
+        """
+        wsclean_opts = "wsclean "
+        for key, val in self.skymodel_kwargs.items():
+            if isinstance(val, list):
+                wsclean_opts += f"-{key} {val[0]} {val[1]} "
+            else:
+                wsclean_opts += f"-{key} {val} "
+
+        wsclean_opts += str(self.ms)
+        sp.run(wsclean_opts, shell=True)
+
+        print(f"Saved to {self.skymodel_kwargs['name']}<...>.fits")
+
+    def clean_image(self) -> None:
+        """Cleans the image using WSClean."""
+        self.clean_config["name"] += "_" + self.clean_config["pol"]
+
+        wsclean_opts = "wsclean "
+        for key, val in self.clean_config.items():
+            if isinstance(val, list):
+                wsclean_opts += f"-{key} {val[0]} {val[1]} "
+            else:
+                wsclean_opts += f"-{key} {val} "
+
+        wsclean_opts += self.ms
+        sp.run(wsclean_opts, shell=True)
+
+        print(f"Saved to {self.clean_config['name']}<...>.fits")
+
+    def _save_config(self, clean_config: dict, output_file: bool | str | Path) -> None:
+        """Saves the config if ``save_config`` is set to ``True``
+        when initializing the class.
+        """
+        if not isinstance(output_file, (str, Path)):
+            output_file = Path(clean_config["file_name"]).name
+            output_file += f"_{clean_config['pol']}" + "_config.toml"
+
+        with open(output_file, "w") as toml_file:
+            toml.dump(clean_config, toml_file)
