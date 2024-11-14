e8eae4d87910dc4fdd99768761ad3b62d7e45fa7
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-11-14 15:09:28 +0100

    Add docstring

--- radiotools/plotting/utils.py
+++ radiotools/plotting/utils.py
@@ -7,7 +7,39 @@ _PREFIXES.update(
 _PREFIXES.update(dict.fromkeys(["arcsecond", "arcsec", "asec", "as"], (3.6e3, "as")))
 
 
-def px2radec(header, xlim=None, ylim=None, num_ticks=9, unit="mas", ax=None):
+def px2radec(
+    header: dict,
+    xlim: tuple | None = None,
+    ylim: tuple | None = None,
+    num_ticks: int | tuple = 9,
+    unit: str = "mas",
+    ax=None,
+):
+    """Converts pixels to RA/Dec relative to source reference pixel.
+    Expects the pixel increment to be in units of deg.
+
+    Parameters
+    ----------
+    header : dict or astropy.io.fits.header.Header
+        Header corresponding to the data.
+    xlim : tuple or None, optional
+        Set the x-axis view limits. If set to ``None``, the full
+        axis size is used. Default: ``None``
+    ylim : tuple or None, optional
+        Set the y-axis view limits. If set to ``None``, the full
+        axis size is used. Default: ``None``
+    num_ticks : int or tuple, optional
+        Number of ticks on the axes. If given a tuple,
+        element 0 correponds to the number of ticks on the
+        x-axis, while element 1 corresponds to the y-axis.
+    unit : str, optional
+        The unit of the ticks. Can be one of ``'microarcsecond'``,
+        ``'µas'``, ``'milliarcsecond'``, ``'marcsec'``, ``'masec'``, ``'mas'``,
+        ``'arcsecond'``, ``'arcsec'``, ``'asec'``, ``'as'``. Default: ``'mas'``
+    ax : matplotlib.axis.Axis, optional
+        Matplotlib axis object to apply the conversion to. This will call
+        the ``ax.set()`` method. Default: ``None``
+    """
     if unit not in _PREFIXES.keys():
         raise ValueError(f"Unknown unit! Please provide one of {_PREFIXES.keys()}")
 
