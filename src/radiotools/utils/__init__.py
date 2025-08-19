9c0ddf2df59151f9be5ad45c588fcef89942b9b1
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2025-08-19 14:32:58 +0200

    add fits2ms and rmtree

--- src/radiotools/utils/__init__.py
+++ src/radiotools/utils/__init__.py
@@ -1,7 +1,9 @@
-from .utils import get_array_names, img2jansky, rms
+from .utils import get_array_names, img2jansky, rms, uvfits2ms, rmtree
 
 __all__ = [
     "get_array_names",
     "img2jansky",
     "rms",
+    "uvfits2ms",
+    "rmtree",
 ]
--- src/radiotools/utils/utils.py
+++ src/radiotools/utils/utils.py
@@ -2,9 +2,11 @@ import re
 
 import numpy as np
 import requests
+import subprocess
 from astropy.io import fits
 from bs4 import BeautifulSoup
 from numpy.typing import ArrayLike
+from pathlib import Path
 
 
 def get_array_names(url: str) -> list[str]:
@@ -73,3 +75,48 @@ def img2jansky(image: ArrayLike, header: fits.Header):
         * np.power(header["CDELT1"], 2)
         / (np.pi * header["BMIN"] * header["BMAJ"])
     )
+
+
+def uvfits2ms(fits_path, ms_path):
+    """Converts a uvfits_file into a measurement set (ms file).
+
+    Parameters
+    ----------
+        fits_path : str
+            path to fits_file
+        ms_path : str
+            path to store ms_file
+    """
+    casa = "casa --quiet --nologger --nologfile --nogui --norc --agg -c "
+    arg = (
+        "importuvfits(fitsfile='"
+        + str(fits_path)
+        + "'"
+        + ", vis="
+        + "'"
+        + str(ms_path)
+        + "'"
+        + ")"
+    )
+
+    command = casa + '"' + arg + '"'
+
+    subprocess.run(command, shell=True)
+
+
+def rmtree(root: Path):
+    """Recursively remove directories and files
+    starting from root directory.
+
+    Parameters
+    ----------
+    root : Path
+        Root path of the directories you want to delete.
+    """
+    for p in root.iterdir():
+        if p.is_dir():
+            rmtree(p)
+        else:
+            p.unlink()
+
+    root.rmdir()
